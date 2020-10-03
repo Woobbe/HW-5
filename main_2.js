ddbf72a811028380c854a3e6dc5b7e207a289448
Shevtsov Vitalii <woobbe@gmail.com>
2020-10-03 23:59:24 +0300

    task 2 - DONE

---
+++ main_2.js
@@ -0,0 +1,60 @@
+function init() {
+
+    let password = '123qwerty';
+
+    function validatePassword(pass) {
+
+        let isValid = strLength(pass) && containsLetter(pass) && containsNum(pass) && !containsNotLetterNum(pass);
+        let resultValidation = (isValid) ? 'VALID' : 'INVALID';
+
+        return console.log(`Password: ${resultValidation}`);
+    };
+
+
+    let strLength = (pass) => (pass.length > 6 && pass.length <= 20);
+
+
+    let containsLetter = (pass) => {
+        for (let char of pass) {
+            let unicodeChar = char.charCodeAt();
+            if ((unicodeChar >= 1040 && unicodeChar <= 1103) || (unicodeChar >= 65 && unicodeChar <= 90 || unicodeChar >= 97 && unicodeChar <= 122)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+
+    let containsNum = (pass) => {
+        for (let num of pass) {
+            let unicodeNum = num.charCodeAt();
+            if ((unicodeNum >= 48 && unicodeNum <= 57)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+
+    let containsNotLetterNum = (pass) => {
+        for (let symb of pass) {
+            let unicodeSymb = symb.charCodeAt();
+            if ((unicodeSymb >= 48 && unicodeSymb <= 57) || (unicodeSymb >= 1040 && unicodeSymb <= 1103) || (unicodeSymb >= 65 && unicodeSymb <= 90 || unicodeSymb >= 97 && unicodeSymb <= 122)) {
+                continue;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    };
+
+
+    validatePassword(password);
+};
+
+
+init();
+
+
+
+
