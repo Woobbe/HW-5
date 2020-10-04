fa3ea9c49c16f0f46a7519b5b7d6caeee09b012b
Shevtsov Vitalii <woobbe@gmail.com>
2020-10-04 11:45:46 +0300

    task 3 DONE

---
+++ main_3.js
@@ -0,0 +1,33 @@
+function init() {
+
+    let numberToCheck = -13;
+
+    function checkNumber(number) {
+
+        let checkArray = [isSimpleNumber(number), multipleNumber(number, 2), multipleNumber(number, 10)];
+        console.log(checkArray);
+    };
+
+
+    function isSimpleNumber(number) {
+        if (Math.sign(number) === -1) {
+            return false;
+        } else {
+            for (let num = 2; num < number; num++) {
+                if (!(number % num)) return false;
+            }
+        }
+        return true;
+    };
+
+
+    function multipleNumber(number, multiple) {
+        return !(number % 10);
+    };
+
+
+    checkNumber(numberToCheck);
+
+};
+
+init();
