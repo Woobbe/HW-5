cca0bc9987aa4b3f7cb334d17833646704149d36
Shevtsov Vitalii <woobbe@gmail.com>
2020-10-11 14:08:55 +0300

    task_3 fixed

--- main_3.js
+++ main_3.js
@@ -1,34 +1,56 @@
 function init() {
 
-    let numberToCheck = 20;
+    let numberToCheck = 5;
+    let checkArray = [];
 
     function checkNumber(number) {
+        if (number === 2) {
+            checkArray.push(true, true, false);
+            console.log(checkArray);
+            return;
+        };
 
-        let checkArray = [isSimpleNumber(number), multipleNumber(number, 2), multipleNumber(number, 10)];
+        if (isSimpleNumber(numberToCheck)) {
+            checkArray.push(true, false, false);
+        } else {
+            checkArray.push(false);
+            multipleTenNumber(number);
+
+        };
         console.log(checkArray);
-    };
+    }
 
 
     function isSimpleNumber(number) {
 
-        if (Math.sign(number) === -1) {
+        if (number < 0) {
             return false;
         } else {
             for (let num = 2; num < number; num++) {
                 if (!(number % num)) return false;
             }
-        }
+        };
         return true;
     };
 
 
-    function multipleNumber(number, multiple) {
-        return !(number % multiple);
+    function multipleTenNumber(number) {
+        if (!(number % 10)) {
+            checkArray.push(true, true);
+        } else {
+            checkEvenNumber(number);
+            checkArray.push(false);
+        };
     };
 
-
+    function checkEvenNumber(number) {
+        if (number & 1) {
+            checkArray.push(false);
+        } else {
+            checkArray.push(true);
+        };
+    };
     checkNumber(numberToCheck);
-
 };
 
 init();
