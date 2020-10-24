f5b4059d073262d2362ed74da1ad1b46b8501961
Vitalii Shevtsov <woobbe@gmail.com>
2020-10-24 21:50:00 +0300

    code style edit

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
 function init() {
-
     const num1 = '999';
-    const num2 = '99999';
-
-
-    function sumString(num1, num2) {
-
-        let num1Array = num1.split('');
-        let num2Array = num2.split('');
-
-        let maxLength = Math.max(num1Array.length, num2Array.length);
-
-        let result = '';
-        let sum = 0;
-        let ten = 0;
-        for (let i = 0; i < maxLength; i++) {
-            let num1LastSymb = ~~num1Array.pop();
-            let num2LastSymb = ~~num2Array.pop();
-
-            sum = num1LastSymb + num2LastSymb + ten;
-
-            if (sum > 9) {
-                sum = sum - 10;
-                ten = 1;
-            } else {
-                ten = 0;
-            }
-
-            result = sum + result;
-        }
-        ;
-
-        if (ten === 1) {
-        result = '1' + result;
-        }
-
-        return result;
-    };
+    const num2 = '999';
 
     console.log(sumString(num1, num2));
-};
+}
+
+function sumString(num1, num2) {
+    let num1Array = num1.split('');
+    let num2Array = num2.split('');
+    let result = '';
+    let sum = 0;
+    let ten = 0;
+
+    let maxLength = Math.max(num1Array.length, num2Array.length);
+
+    for (let i = 0; i < maxLength; i++) {
+        let num1LastSymb = ~~num1Array.pop();
+        let num2LastSymb = ~~num2Array.pop();
+
+        sum = num1LastSymb + num2LastSymb + ten;
+
+        if (sum > 9) {
+            sum = sum - 10;
+            ten = 1;
+        } else {
+            ten = 0;
+        }
+
+        result = sum + result;
+    }
+
+    if (ten === 1) {
+        result = '1' + result;
+    }
+
+    return result;
+}
 
 
 init();
--- main_2.js
+++ main_2.js
@@ -1,60 +1,57 @@
-function init() {
+let password = '123+qwerty';
 
-    let password = '123+qwerty';
+function validatePassword(password) {
+    let isValid = strLength(password)
+        && containsLetter(password)
+        && containsNum(password)
+        && !containsNotLetterNum(password);
+    let resultValidation = (isValid) ? 'VALID' : 'INVALID';
 
-    function validatePassword(password) {
+    return console.log(`Password: ${resultValidation}`);
+}
 
-        let isValid = strLength(password) && containsLetter(password) && containsNum(password) && !containsNotLetterNum(password);
-        let resultValidation = (isValid) ? 'VALID' : 'INVALID';
+let strLength = (password) => (password.length > 6 && password.length <= 20);
 
-        return console.log(`Password: ${resultValidation}`);
-    };
+let containsLetter = (password) => {
+    for (let char of password) {
+        let getUnicodeChar = char.charCodeAt();
 
+        let isLetterA_я = getUnicodeChar >= 1040 && getUnicodeChar <= 1103;
+        let isLetterA_Z = getUnicodeChar >= 65 && getUnicodeChar <= 90;
+        let isLetter_a_z = getUnicodeChar >= 97 && getUnicodeChar <= 122;
 
-    let strLength = (password) => (password.length > 6 && password.length <= 20);
+        if (isLetterA_я || isLetterA_Z || isLetter_a_z) return true;
+    }
 
-
-    let containsLetter = (password) => {
-        for (let char of password) {
-            let getUnicodeChar = char.charCodeAt();
-            if ((getUnicodeChar >= 1040 && getUnicodeChar <= 1103) || (getUnicodeChar >= 65 && getUnicodeChar <= 90 || getUnicodeChar >= 97 && getUnicodeChar <= 122)) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-
-    let containsNum = (password) => {
-        for (let number of password) {
-            let getUnicodeNum = number.charCodeAt();
-            if ((getUnicodeNum >= 48 && getUnicodeNum <= 57)) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-
-    let containsNotLetterNum = (password) => {
-        for (let symbol of password) {
-            let getUnicodeSymbol = symbol.charCodeAt();
-            let number = (getUnicodeSymbol >= 48 && getUnicodeSymbol <= 57);
-            let letter = (getUnicodeSymbol >= 1040 && getUnicodeSymbol <= 1103) || (getUnicodeSymbol >= 65 && getUnicodeSymbol <= 90 || getUnicodeSymbol >= 97 && getUnicodeSymbol <= 122);
-            if (!number && !letter) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-
-    validatePassword(password);
+    return false;
 };
 
+let containsNum = (password) => {
+    for (let number of password) {
+        let getUnicodeNum = number.charCodeAt();
 
-init();
+        let isNumber = getUnicodeNum >= 48 && getUnicodeNum <= 57;
 
+        if (isNumber) return true;
+    }
 
+    return false;
+};
 
+let containsNotLetterNum = (password) => {
+    for (let symbol of password) {
+        let getUnicodeSymbol = symbol.charCodeAt();
 
+        let isLetterA_я = getUnicodeSymbol >= 1040 && getUnicodeSymbol <= 1103;
+        let isLetterA_Z = getUnicodeSymbol >= 65 && getUnicodeSymbol <= 90;
+        let isLetter_a_z = getUnicodeSymbol >= 97 && getUnicodeSymbol <= 122;
+        let isNumber = (getUnicodeSymbol >= 48 && getUnicodeSymbol <= 57);
+        let isLetter = isLetterA_я || isLetterA_Z || isLetter_a_z;
+
+        if (!isNumber && !isLetter) return true;
+    }
+
+    return false;
+};
+
+validatePassword(password);
